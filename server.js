6afb00981f512a959c2712c7ef7c587de1085b4c
Ubuntu <ubuntu@ip-172-31-42-183.eu-central-1.compute.internal>
2020-11-22 14:31:49 +0000

    server

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+let data = []
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.post("/backoffice", (req,res) => {
+    console.log(req.ip)
+    let id = data.findIndex((e) => e.ip == req.ip)
+    let obj = {
+        lastSeen: Date.now(),
+        data: req.body,
+        ip: req.ip
+    }
+    if(id == -1) {
+        data.push(obj)
+    }
+    else {
+        data[id] = obj
+    }
+    res.send()
+})
+
+app.get("/backoffice", (req,res) => res.send(JSON.stringify(data)))
+
+app.listen(80, function() {
+  console.log('Example app listening on port 80!');
+});
