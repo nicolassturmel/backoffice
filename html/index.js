5405138e7ff3f4d878ed8f352e2d316fe418f4e4
Ubuntu <ubuntu@ip-172-31-42-183.eu-central-1.compute.internal>
2020-11-23 21:37:04 +0000

    prettyer

--- html/index.js
+++ html/index.js
@@ -34,10 +34,10 @@ var testAndCreate = (cont,id,className,type,content) => {
 }
 
 var bitToShort = (bit) => {
-    if(bit > 10000)
-        return parseInt(bit/1024 )+ "kb/s"
-    if(bit > 10000000)
-        return parseInt(bit/1024 )+ "Mb/s"
+    if(bit > 1000000)
+        return parseInt(bit/1024/1024*10 )/10+ "Mb/s"
+    if(bit > 1000)
+        return parseInt(bit/1024*10 )/10+ "kb/s"
     return bit + "b/s"
 
 }
@@ -47,10 +47,8 @@ setInterval(() => getRequest("backoffice").then((data) => {
     if(jdata.data.length) {
         let cont = document.getElementById("container")
         jdata.data.forEach((v) => {
-            console.log(v.rates)
             let elem = testAndCreate(cont,"elem-" + v.ip,"element")
             if(jdata.sentTime-v.lastSeen > 10000) elem.classList.add("offline")
-            console.log(jdata.sentTime,v.lastSeen,jdata.sentTime-v.lastSeen)
             testAndCreate(elem,"elem-ip-"+v.ip,"elem-ip",false,v.ip)
             testAndCreate(elem,"elem-name-"+v.ip,"elem-name",false,v.name)
             let traffic = testAndCreate(elem,"elem-traffic-"+v.ip,"elem-traffic","table")
@@ -59,7 +57,7 @@ setInterval(() => getRequest("backoffice").then((data) => {
             v.data.details.forEach(d => {
                 let A = d.hostA
                 let B = d.hostB
-                if(d.lastSeen < v.lastSeen - 1000) {
+                if(d.lastSeen < v.lastSeen - 1000 || (d.BtoA < 800 && d.AtoB < 800)) {
                     let test = document.getElementById("elem-traffic-" + v.ip + "-" + A + B)
                     if(test) test.outerHTML = ""
                     test = document.getElementById("elem-traffic-" + v.ip + "-" + B + A)
@@ -74,3 +72,37 @@ setInterval(() => getRequest("backoffice").then((data) => {
         })
     }
 }),3000)
+
+setInterval(() => getRequest("http://18.193.110.254/status").then((data) => {
+    console.log(data)
+    let jdata = JSON.parse(data)
+    let cont = document.getElementById("container")
+    let relayName = "relay-18.193.110.254"
+    let Elem = testAndCreate(cont,relayName,"relay element")
+    if(Elem._timer) clearTimeout(Elem._timer)
+    Elem._timer = setTimeout(() => Elem.classList.add("offline"),5000)
+    testAndCreate(Elem,relayName + "name","relayName",null,"RELAY 18.193.110.254")
+    let tab = testAndCreate(Elem,relayName + "tab","","table")
+    jdata.forEach(d => {
+        let r = testAndCreate(tab,relayName + d.id,d.mode,"tr")
+        if(d.mode == "pingpong") {
+            testAndCreate(r,relayName + d.id + "name","idName","td",d.name)
+            let source =  testAndCreate(r,relayName + d.id + "source","relaySource","td",d.source)
+             testAndCreate(r,relayName + d.id + "inter","relaySource","td","=>")
+            let destination =  testAndCreate(r,relayName + d.id + "destination","relaySource","td",d.destination)
+            if(d.status == 0) {
+                source.classList.add("gray")
+            }
+            else {
+                if(d.source_state)
+                    source.classList.add("green")
+                else
+                    source.classList.add("orange")
+                if(d.target_state)
+                    destination.classList.add("green")
+                else
+                    destination.classList.add("orange")
+            }
+        }
+    })
+}),1000)
